ddb11a2d88824276417a59ddf3220fbd51e2be4b
wkj <573807412@qq.com>
2019-01-24 07:19:46 +0800

    test

--- Application/Home/Controller/CommonController.class.php
+++ Application/Home/Controller/CommonController.class.php
@@ -3,7 +3,7 @@
 namespace Home\Controller;
 use Think\Controller;
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Credentials : true");
+header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Connection, User-Agent, Cookie");
 class CommonController extends Controller {
 
