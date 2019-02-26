0dcb210dd0fcd186f78242664ad1bfcc36047c5f
wkj <573807412@qq.com>
2019-02-26 08:58:24 +0800

    bug

--- Application/Home/Controller/CategoryController.class.php
+++ Application/Home/Controller/CategoryController.class.php
@@ -9,6 +9,12 @@ class CategoryController extends CommonController {
         $this->categoryService = CS('Category');//定义使用service
     }
 
+    //内部获取栏目信息
+    private function _getCategorys($condition = []){
+        $condition['status'] = 1;
+        return $this->categoryService->getCategorys($condition);//获取生效的栏目
+    }
+
     //获取栏目信息(按照子菜单形式)
     public function getCategorys(){
         $data = $this->_getCategorys();
@@ -19,16 +25,21 @@ class CategoryController extends CommonController {
         }
     }
 
-    //内部获取栏目信息
-    private function _getCategorys(){
+    public function getCategorysByOrCondition(){
         $condition = I('get.');
-        $condition['status'] = 1;
-        return $this->categoryService->getCategorys($condition);//获取生效的栏目
+        $condition['_logic'] = 'or';
+        $map['_complex'] = $condition;
+        $data = $this->_getCategorys($condition);
+        if(!is_bool($data)){
+            $this->ajaxSuccess(toLayer($data), '获取栏目列表成功');
+        }else{
+            $this->ajaxError([], '获取栏目列表失败');
+        }
     }
 
     //获取栏目信息（根据条件）
     public function getCategorysByCondition(){
-        $data = $this->_getCategorys();
+        $data = $this->_getCategorys(I('get.'));
         if(!is_bool($data)){
             $this->ajaxSuccess($data, '获取栏目列表成功');
         }else{
