196efa6ce547461e3cbcf2547f3cf4048c1ef048
wangkj <wangkj@yex360.com>
2019-09-11 03:44:26 +0800

    上线

--- Application/Home/Controller/CommonController.class.php
+++ Application/Home/Controller/CommonController.class.php
@@ -2,7 +2,7 @@
 //前端的公共类
 namespace Home\Controller;
 use Think\Controller;
-header("Access-Control-Allow-Origin: http://test.ecm-fans.com");
+header("Access-Control-Allow-Origin: http://www.ecm-fans.com");
 //header("Access-Control-Allow-Origin: http://127.0.0.1:5500");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Connection, User-Agent, Cookie");
